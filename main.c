bddbcbe3c992950fa8e4c54dde3c385c7a95603d
Oscar Urenda Moix <124069546+oscarurenda2001@users.noreply.github.com>
2025-04-25 16:55:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,108 @@
-/*
- * bsp_i2c.h
- *
- *  Created on: 02 de abr. de 2023
- *      Author: Marius Monton
- */
-#ifndef INC_BSP_I2C_H_
-#define INC_BSP_I2C_H_
 
-#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * @brief Initializes I2C subsystem
- * @param addr I2C address of device to access
- */
-void BSP_I2C_Init(uint8_t addr);
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+#include "croutine.h"
 
-/**
- * @brief Write register using default I2C bus
- * @param reg register to write
- * @param data data to write
- * @return true on success
- */
-bool BSP_I2C_WriteRegister(uint8_t reg, uint8_t data);
+#include "em_chip.h"
+#include "bsp.h"
+#include "bsp_trace.h"
 
-/**
- * @brief Read register from I2C device
- * @param reg Register to read
- * @param val Value read
- * @return true on success
- */
-bool BSP_I2C_ReadRegister(uint8_t reg, uint8_t *val);
-bool BSP_I2C_ReadRegister16(uint8_t reg, uint16_t *val);
-
-/**
- * @brief Test function
- */
-bool I2C_Test();
+#include "bsp_i2c.h"
 
 
+#define STACK_SIZE_FOR_TASK	(configMINIMAL_STACK_SIZE + 10)
+#define TASK_PRIORITY      	(tskIDLE_PRIORITY + 1)
+QueueHandle_t xQueue;
+/***************************************************************************//**
+ * @brief Simple task which is blinking led
+ * @param *pParameters pointer to parameters passed to the function
+ ******************************************************************************/
+struct Message
+{
+	int contador;
+	int numero;
+}Message;
 
+static void LedBlink(void *pParameters)
+{
+  (void) pParameters;
+  const portTickType delay = pdMS_TO_TICKS(500);
+  for (;; ) {
+	BSP_LedToggle(1);
+	Message.contador += 1;
+	Message.numero = 10;
+	//printf("Task 1\n");
+	xQueueSend(xQueue, (void *) &Message, 1000);
+	/*printf("Counter %d \n", Message.contador);
+	printf("Lletra %d \n", Message.numero);*/
+	vTaskDelay(delay);
+  }
+}
 
+static void LedBlink2(void *pParameters)
+{
+  (void) pParameters;
+  const portTickType delay = pdMS_TO_TICKS(1000);
+  struct Message valor;
+  for (;; ) {
+	BSP_LedToggle(0);
+	//printf("Task 2\n");
+	xQueueReceive(xQueue, &(valor), 1000);
+	Message.contador = Message.contador + 1;
+	xQueueSend(xQueue, (void *) &Message, 1000);
+	vTaskDelay(delay);
+  }
+}
+static void LedBlink3(void *pParameters)
+{
+  (void) pParameters;
+  const portTickType delay = pdMS_TO_TICKS(2000);
+  struct Message valor;
+  for (;; ) {
+	BSP_LedToggle(0);
+	//printf("Task 2\n");
+	xQueueReceive(xQueue, &(valor), 1000);
+	/*printf("Lecture %d \n", valor.contador );
+	printf("Lecture lletra %d \n", valor.numero);*/
+	vTaskDelay(delay);
+  }
+}
 
-#endif /* INC_BSP_I2C_H_ */
+/***************************************************************************//**
+ * @brief  Main function
+ ******************************************************************************/
+int main(void)
+{
+  /* Chip errata */
+  CHIP_Init();
+  /* If first word of user data page is non-zero, enable Energy Profiler trace */
+  BSP_TraceProfilerSetup();
+  BSP_I2C_Init(0x52);
+  /* Initialize LED driver */
+  BSP_LedsInit();
+  /* Setting state of leds*/
+  BSP_LedSet(0);
+  BSP_LedSet(1);
+  xQueue = xQueueCreate( 10, sizeof( struct Message) );
+  /*Create two task for blinking leds*/
+  xTaskCreate(LedBlink, (const char *) "LedBlink1", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
+  xTaskCreate(LedBlink2, (const char *) "LedBlink2", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
+  xTaskCreate(LedBlink3, (const char *) "LedBlink3", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
+
+  /*Start FreeRTOS Scheduler*/
+  uint16_t distance;
+  if (BSP_I2C_ReadRegister16(0x10, &distance)) {
+      printf("Distancia: %d mm\n", distance);
+  } else {
+      printf("Error al leer distancia\n");
+  }
+  vTaskStartScheduler();
+
+  return 0;
+}
